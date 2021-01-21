3e038dd9ca69635269b4a1fc21768b24b7a7fb76
master <rudolifrudolif@gmail.com>
2021-01-21 18:37:29 +0200

    finall

--- app/js/app.js
+++ app/js/app.js
@@ -3,30 +3,75 @@
 document.addEventListener("DOMContentLoaded", function() {
 
 	//----------------------typePassword-----------------------
-	const typePassword = (typeSelector) => {
-		let type = document.querySelectorAll(typeSelector);
+		const typePassword = (typeSelector) => {
+			let type = document.querySelectorAll(typeSelector);
 
-		type.forEach(item => {
-			let buttonSelector = item.querySelector('button');
-			let inputSelector = item.querySelector('input');
+			type.forEach(item => {
+				let buttonSelector = item.querySelector('button');
+				let inputSelector = item.querySelector('input');
 
-			buttonSelector.addEventListener('click', function(e) {
-					e.preventDefault();
-					buttonSelector.classList.toggle('hidden');
-					inputSelector.setAttribute('type', 'password');
-					inputSelector.classList.toggle('text');
+				buttonSelector.addEventListener('click', function(e) {
+						e.preventDefault();
+						buttonSelector.classList.toggle('hidden');
+						inputSelector.setAttribute('type', 'password');
+						inputSelector.classList.toggle('text');
 
-					let classText = item.querySelector('.text');
+						let classText = item.querySelector('.text');
 
-					if (classText) {
-						item.querySelector('.text').setAttribute('type', 'text');
-					}
+						if (classText) {
+							item.querySelector('.text').setAttribute('type', 'text');
+						}
 
 
+				});
 			});
-		});
-	};
-	typePassword('.typePassword');
+		};
+		typePassword('.typePassword');
+
+	//----------------------TABS-JS----------------------
+		const tabs = (headerSelector, tabSelector, contentSelector, activeClass) => {
+			let check = document.querySelector(headerSelector);
+
+			if (check) {
+				const header = document.querySelector(headerSelector),
+							tab = document.querySelectorAll(tabSelector),
+							content = document.querySelectorAll(contentSelector);
+			
+				function hideTabContent() {
+					content.forEach(item => {
+						item.style.display = "none";
+					});
+			
+					tab.forEach(item => {
+						item.classList.remove(activeClass);
+					});
+				}
+			
+				function showTabContent(i = 0) {
+					content[i].style.display = "block";
+					tab[i].classList.add(activeClass);
+				}
+			
+				hideTabContent();
+				showTabContent();
+			
+				header.addEventListener('click', (e) => {
+					e.preventDefault();
+					const target = e.target;
+					if (target && 
+						(target.classList.contains(tabSelector.replace(/\./, '')) || 
+						target.parentNode.classList.contains(tabSelector.replace(/\./, '')))) {
+						tab.forEach((item, i) => {
+							if (target == item || target.parentNode == item) {
+								hideTabContent();
+								showTabContent(i);
+							}
+						});
+					}
+				});
+			}
+		};
+		tabs('.tabs', '.tabs__item', '.tabs__wrap', 'section__bot_nav-active');
 
 
 });
